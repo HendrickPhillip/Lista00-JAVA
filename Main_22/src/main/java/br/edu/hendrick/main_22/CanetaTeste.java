c8d5db987522acb9793afdebf0bd61ee6b11ecd0
HendrickPhillip <105526467+HendrickPhillip@users.noreply.github.com>
2022-10-02 18:28:17 -0300

    Add files via upload

---
+++ Main_22/src/main/java/br/edu/hendrick/main_22/CanetaTeste.java
@@ -0,0 +1,7 @@
+package br.edu.hendrick.main_22;
+public class CanetaTeste {
+    public void teste(){
+        Caneta caneta1 = new Caneta(), caneta2 = new Caneta("Gillete", "Vermelho", 0.25);
+        System.out.println(caneta1);System.out.println(caneta2);
+    }
+}
---
+++ Main_22/src/main/java/br/edu/hendrick/main_22/Main_22.java
@@ -0,0 +1,8 @@
+package br.edu.hendrick.main_22;
+public class Main_22 {
+
+    public static void main(String[] args) { 
+        CanetaTeste ct = new CanetaTeste();
+        ct.teste();
+    }
+}
