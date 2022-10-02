1b7ef31d5cac7990ce70e7107d0a2efc4035698d
HendrickPhillip <105526467+HendrickPhillip@users.noreply.github.com>
2022-10-02 18:27:37 -0300

    Add files via upload

---
+++ Main_20/src/main/java/br/edu/hendrick/main_20/Pessoa.java
@@ -0,0 +1,31 @@
+package br.edu.hendrick.main_20;
+public class Pessoa {
+    private static int contadorID = 0;
+    private int ID;
+    private double peso;
+
+    public Pessoa(){
+        this.peso = 70.0;
+        ID = geraId();
+    }
+    public Pessoa(double peso){
+        this.peso = peso;
+        ID = geraId();
+    }
+
+    public double getID() {
+        return ID;
+    }
+
+    public double getPeso() {
+        return peso;
+    }
+    public void setPeso(double peso) {
+        this.peso = peso;
+    }
+
+    private int geraId(){
+        contadorID += 1;
+        return contadorID;
+    }
+}
